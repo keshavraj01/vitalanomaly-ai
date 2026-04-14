4eaacd25488e0dec5af15e4a34aa99e29ec1c650
keshavraj01 <keshavrajora122@gmail.com>
2026-04-14 13:33:57 +0530

    Prepared backend for deployment

--- backend/main.py
+++ backend/main.py
@@ -226,3 +226,9 @@ async def upload_file(file: UploadFile = File(...)):
         "health_summary": dict(Counter(labels)),
         "plots": plots
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
